52f02b2352d8baf45a665c8cd7168c63494c16b8
DQing <15829258737@163.com>
2017-05-02 16:23:53 +0800

    add some method

--- program.js
+++ program.js
@@ -1,6 +1,32 @@
 function calculateScore(input) {
-    let result = '167';
+    let result;
+    let inputArray = input.split('|');
+    for (let i = 0; i < inputArray.length; i++) {
+        chooseMethod(inputArray[i]);
+    }
+
     console.log(result);
+}
+
+function chooseMethod(score) {
+    let scoreArray = score.split('');
+    if (scoreArray.indexOf('X') != -1) {
+        calculateStrike();
+    } else if (scoreArray.indexOf('/') != -1) {
+        calculateSpare();
+    } else if (scoreArray.indexOf('-') != -1) {
+        calculateMiss();
+    }
+}
+
+function calculateStrike() {
+
+}
+function calculateSpare() {
+
+}
+function calculateMiss() {
+
 }
 
 
